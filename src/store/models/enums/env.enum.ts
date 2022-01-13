6a1a790852c33781c9ba8f75939e57c1a696f94f
Zhora Sargsyan <jorsargsyan28@gmail.com>
2022-01-13 10:32:31 +0400

    fix:removed qa base urls

--- src/store/models/enums/env.enum.ts
+++ src/store/models/enums/env.enum.ts
@@ -1,8 +1,8 @@
 // export enum EBaseUrl {
-//   envApiIdentity = 'https://identityqa.devhaypost.com/Api',
-//   envApiCustomer = 'https://customerqa.devhaypost.com/Api',
-//   envApiLogistics = 'https://logisticsqa.devhaypost.com/Api',
-//   envApiOrder = 'https://orderqa.devhaypost.com/Api',
+//   envApiIdentity = 'https://identity.devhaypost.com/Api',
+//   envApiCustomer = 'https://customer.devhaypost.com/Api',
+//   envApiLogistics = 'https://logistics.devhaypost.com/Api',
+//   envApiOrder = 'https://order.devhaypost.com/Api',
 // }
 
 export enum EBaseUrl {
