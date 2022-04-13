e4be7bd554a0ab41692209d5b94075e3e988d4a7
Zhora Sargsyan <jorsargsyan28@gmail.com>
2022-04-13 16:42:10 +0400

    fix: changed api urls

--- src/store/models/enums/env.enum.ts
+++ src/store/models/enums/env.enum.ts
@@ -1,13 +1,13 @@
-export enum EBaseUrl {
-  envApiIdentity = 'https://identity.devhaypost.com/Api',
-  envApiCustomer = 'https://customer.devhaypost.com/Api',
-  envApiLogistics = 'https://logistics.devhaypost.com/Api',
-  envApiOrder = 'https://order.devhaypost.com/Api',
-}
-
 // export enum EBaseUrl {
-//   envApiIdentity = 'https://id.devhaypost.com/Api',
-//   envApiCustomer = 'https://cu.devhaypost.com/Api',
-//   envApiLogistics = 'https://lg.devhaypost.com/Api',
-//   envApiOrder = 'https://or.devhaypost.com/Api',
+//   envApiIdentity = 'https://identity.devhaypost.com/Api',
+//   envApiCustomer = 'https://customer.devhaypost.com/Api',
+//   envApiLogistics = 'https://logistics.devhaypost.com/Api',
+//   envApiOrder = 'https://order.devhaypost.com/Api',
 // }
+
+export enum EBaseUrl {
+  envApiIdentity = 'https://id.devhaypost.com/Api',
+  envApiCustomer = 'https://cu.devhaypost.com/Api',
+  envApiLogistics = 'https://lg.devhaypost.com/Api',
+  envApiOrder = 'https://or.devhaypost.com/Api',
+}
